bac7b29c060d5cf087afb638aabfdb89ec990dd0
githubaccount-2022 <shravanjerry2001@gmail.com>
2023-05-07 23:57:17 +0530

    final

--- weatherdata/views.py
+++ weatherdata/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render , HttpResponse , redirect
 
 # Create your views here.
-
-
+import pgeocode
+nomi = pgeocode.Nominatim('in')
 def index(request):
     # x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
     # if x_forwarded_for:
@@ -17,8 +17,11 @@ def index(request):
 def home(request):
     if request.method == 'POST':
         city = request.POST.get('cityname')
-        print(city)
-        return render(request,'home.html',{'city':city})
+        if city.isnumeric():
+            loc = nomi.query_postal_code(city).place_name.split(',')[0]
+            return render(request,'home.html',{'city': loc})
+        else:
+            return render(request,'home.html',{'city':city})
     else:
         return render(request,'home.html')
     
